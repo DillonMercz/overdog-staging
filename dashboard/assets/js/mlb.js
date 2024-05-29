47f28e478b596630a8d4e31b622a116c52a53f6f
Dylan Clayton <dylanclayton3@gmail.com>
2024-05-29 16:36:07 -0400

    course update

--- dashboard/assets/js/mlb.js
+++ dashboard/assets/js/mlb.js
@@ -561,7 +561,7 @@ function displayScores(data) {
         }
 
         const cardTemplate = `
-                  <div class="scoreboard-item card h-100">
+                  <div class="scoreboard-item card h-100" onclick="./mlb-game.html?game=${game.gamePK}">
                     <div class="card-body" style="padding-top:5px">
                         <!-- Row 1 -->
                         <div class="row mt-4 align-items-center">
