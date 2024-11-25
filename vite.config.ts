47ae99f3c4a5deeef4a57e8a8ac481df7c85cc69
DillonMercz <dylanclayton3@gmail.com>
2024-11-25 02:49:41 -0500

    Initial commit for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://0.0.0.0:8888',
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  }
+  base: '/overdog-staging/',
+  plugins: [react()]
 })
