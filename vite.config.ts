d8f98c45c6f03ea2f82553ed358d5663ee2639bc
DillonMercz <dylanclayton3@gmail.com>
2024-11-25 13:29:43 -0500

    Configure custom domain staging.overdogbets.com

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/overdog-staging/', // Add base URL for GitHub Pages
+  base: '/', // Updated for custom domain
   plugins: [react()],
   server: {
     proxy: {
